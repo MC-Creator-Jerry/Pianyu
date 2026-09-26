ba22da3d6baf516764e9e4eaf06468acac6e7d01
MC-Creator-Jerry <jerryjwx@163.com>
2026-09-26 11:40:03 +0800

    Update assets/pianyu.js (2026-09-26 11:40)

--- assets/pianyu.js
+++ assets/pianyu.js
@@ -243,7 +243,7 @@ PY.user = {
       (u.promember && u.promember.active ? '<span class="badge">⚡升级-付费成员</span>' : '') +
       '</div><div class="l">' + PY.esc(u.login ? '@' + u.login : '小蓝页账户') + '</div></div>' +
       '<div class="sep"></div>' +
-      '<a role="menuitem" href="creator.html">屿作台</a>' +
+      '<a role="menuitem" href="creator.html">创作岛</a>' +
       '<a role="menuitem" href="profile.html">我的主页</a>' +
       '<a role="menuitem" href="settings.html">设置</a>' +
       '<a role="menuitem" id="afdianItem" href="' + PY.AFDIAN_URL + '" target="_blank" rel="noopener">⚡ 发电支持 · 升级</a>' +
